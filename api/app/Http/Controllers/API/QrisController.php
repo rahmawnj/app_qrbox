449b38ed936ad12e3e5f5447766079a04c95ef3e
Rahma <rahmawnj3@gmail.com>
2026-01-21 16:22:28 +0700

    handling error

--- api/app/Http/Controllers/API/QrisController.php
+++ api/app/Http/Controllers/API/QrisController.php
@@ -313,7 +313,6 @@ class QrisController extends Controller
                 return response()->json([
                     'status'  => 'success',
                     'message' => [
-                        // 'type' => $deviceTransaction->service_type,
                         'order_id'        => $orderId,
                         'payment_status'  => $transactionStatus,
                         'qr_code_deleted' => false,
